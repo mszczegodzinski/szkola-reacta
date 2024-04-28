9b039d2fae072a02d4e9a153716bf2f828583876
Marek Szczegodziński <mszczegodzinski@o2.pl>
2024-04-29 00:44:28 +0200

    feat: 02-03 add fields chages history stored in local storage

--- src/components/FormWizard/FormWizard.tsx
+++ src/components/FormWizard/FormWizard.tsx
@@ -1,6 +1,7 @@
 import { useState, ChangeEvent, FormEvent } from 'react';
 import { FormWizardStep } from './FormWizardStep';
-import { Text } from '../../ui';
+import { Header, Text } from '../../ui';
+import { z } from 'zod';
 
 type FormData = {
   firstName: string;
@@ -8,6 +9,14 @@ type FormData = {
   birthDate: string;
   hobby: string;
 };
+
+const FieldHistorySchema = z.object({
+  fieldName: z.string(),
+  fieldValue: z.string().min(2, 'Value must contains at least 2 characters'),
+  updated: z.string(),
+});
+
+type FieldHistory = z.infer<typeof FieldHistorySchema>;
 
 export const FormWizard = () => {
   const [step, setStep] = useState<number>(1);
@@ -17,6 +26,10 @@ export const FormWizard = () => {
     birthDate: '',
     hobby: '',
   });
+  const [error, setError] = useState<string | null>(null);
+  const [fieldsHistory, setFieldsHistory] = useState<
+    { name: string; history: FieldHistory[] }[]
+  >([]);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -37,48 +50,107 @@ export const FormWizard = () => {
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
-    alert('form submitted');
     console.log('Submitted data:', formData);
+    alert('form submitted');
+  };
+
+  const handleBlur = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    const result = FieldHistorySchema.safeParse({
+      fieldName: name,
+      fieldValue: value,
+      updated: new Date().toLocaleString(),
+    });
+
+    if (!result.success) {
+      setError(result.error?.issues[0].message);
+      return;
+    }
+
+    const fieldHistory = localStorage.getItem(name);
+    const fieldHistoryParsed: FieldHistory[] = fieldHistory
+      ? JSON.parse(fieldHistory)
+      : [];
+    const currentField = {
+      fieldName: name,
+      fieldValue: value,
+      updated: new Date().toLocaleString(),
+    };
+    localStorage.setItem(
+      name,
+      JSON.stringify([...fieldHistoryParsed, currentField]),
+    );
+    setFieldsHistory((prevState) => {
+      const fieldIndex = prevState.findIndex((field) => field.name === name);
+      if (fieldIndex === -1) {
+        return [...prevState, { name, history: [currentField] }];
+      }
+      return prevState.map((fieldHistory) =>
+        fieldHistory.name === name
+          ? { name, history: [...fieldHistory.history, currentField] }
+          : fieldHistory,
+      );
+    });
   };
 
   switch (step) {
     case 1:
       return (
-        <FormWizardStep
-          header="Step 1: Personal Information"
-          step={1}
-          nextStep={nextStep}
-          prevStep={prevStep}
-        >
-          <div className="flex flex-col gap-y-3">
-            <div className="flex gap-4">
-              <label htmlFor="firstName">
-                <Text>First Name:</Text>
-              </label>
-              <input
-                type="text"
-                id="firstName"
-                name="firstName"
-                value={formData.firstName}
-                onChange={handleChange}
-                required
-                className="rounded border border-solid border-slate-800 px-2 py-1"
-              />
+        <>
+          <FormWizardStep
+            header="Step 1: Personal Information"
+            step={1}
+            nextStep={nextStep}
+            prevStep={prevStep}
+          >
+            <div className="flex flex-col gap-y-3">
+              <div className="flex gap-4">
+                <label htmlFor="firstName">
+                  <Text>First Name:</Text>
+                </label>
+                <input
+                  type="text"
+                  id="firstName"
+                  name="firstName"
+                  value={formData.firstName}
+                  onChange={handleChange}
+                  required
+                  className="rounded border border-solid border-slate-800 px-2 py-1"
+                  onBlur={handleBlur}
+                />
+              </div>
+              <div className="flex gap-4">
+                <label htmlFor="lastName">Last Name:</label>
+                <input
+                  type="text"
+                  id="lastName"
+                  name="lastName"
+                  value={formData.lastName}
+                  onChange={handleChange}
+                  required
+                  className="rounded border border-solid border-slate-800 px-2 py-1"
+                  onBlur={handleBlur}
+                />
+              </div>
+              {error && <p style={{ color: 'red' }}>{error}</p>}
             </div>
-            <div className="flex gap-4">
-              <label htmlFor="lastName">Last Name:</label>
-              <input
-                type="text"
-                id="lastName"
-                name="lastName"
-                value={formData.lastName}
-                onChange={handleChange}
-                required
-                className="rounded border border-solid border-slate-800 px-2 py-1"
-              />
-            </div>
+          </FormWizardStep>
+          <div className="px-8">
+            <Header headingLevel="h4">Change history:</Header>
+            <ul>
+              {fieldsHistory.map((field, index) => (
+                <li key={index}>
+                  {field.history.map((history, index) => (
+                    <Text
+                      key={`history-${index}`}
+                    >{`${field.name}: ${history.fieldValue}, updated: ${history.updated}`}</Text>
+                  ))}
+                </li>
+              ))}
+            </ul>
           </div>
-        </FormWizardStep>
+        </>
       );
     case 2:
       return (
